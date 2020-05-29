5bf2de2d453c56970901002589a18d022dd77463
anantakdewi <ananta.kusumadewi@gmail.com>
2020-05-29 20:53:48 +0800

    tambah logo

--- application/views/auth/lupa.php
+++ application/views/auth/lupa.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,68 +26,71 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
+
 <body class="hold-transition register-page">
-<div class="register-box">
-  <div class="register-logo">
-    <b>SIPADU</b> ALUMNI
-    <h4>POLITEKNIK STATISTIKA STIS</h4>
-  </div>
-  
-  <div class="register-box-body">
-  
-    <p class="login-box-msg">Reset Password</p>
-    <?php echo $this->session->flashdata('message'); ?>
+  <div class="register-box">
+    <div class="register-logo">
+      <img src="../assets/dist/img/up.png" class="rounded-center" width="100x">
+      <br>
+      <h1><b>SIPADU</b> ALUMNI</h1>
+      <h4>POLITEKNIK STATISTIKA STIS</h4>
+    </div>
 
-    <form action="<?php echo base_url() ?>auth/forgotPassword" method="post" autocomplete="off">
-      
-      <div class="form-group has-feedback">
-        <input type="text" name="email" class="form-control" placeholder="Email" value="<?php echo set_value('email')?>">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-        <?php echo form_error('email', '<small class="text-danger pl-3">', '</small>');?>
-      </div>
+    <div class="register-box-body">
 
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-             
-            </label>
+      <p class="login-box-msg">Reset Password Pengguna</p>
+      <?php echo $this->session->flashdata('message'); ?>
+
+      <form action="<?php echo base_url() ?>auth/forgotPassword" method="post" autocomplete="off">
+
+        <div class="form-group has-feedback">
+          <input type="text" name="email" class="form-control" placeholder="Email" value="<?php echo set_value('email') ?>">
+          <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          <?php echo form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-8">
+            <div class="checkbox icheck">
+              <label>
+
+              </label>
+            </div>
           </div>
+          <!-- /.col -->
+          <div class="col-xs-4">
+            <button type="submit" name="reset" class="btn btn-primary btn-block btn-flat">Reset</button>
+          </div>
+          <!-- /.col -->
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" name="reset" class="btn btn-primary btn-block btn-flat">Reset</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
+      </form>
 
 
-    <a href="<?php echo base_url('index.php/auth/login') ?>" class="text-center">Saya sudah punya akun</a>
+      <a href="<?php echo base_url('index.php/auth/login') ?>" class="text-center">Saya sudah punya akun</a>
+    </div>
+    <!-- /.form-box -->
   </div>
-  <!-- /.form-box -->
-</div>
-<!-- /.register-box -->
+  <!-- /.register-box -->
 
-<!-- jQuery 2.2.3 -->
-<script src="<?php echo base_url() ?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="<?php echo base_url() ?>assets/plugins/iCheck/icheck.min.js"></script>
-<!-- bootstrap datepicker -->
-<script src="<?php echo base_url() ?>assets/plugins/datepicker/bootstrap-datepicker.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+  <!-- jQuery 2.2.3 -->
+  <script src="<?php echo base_url() ?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
+  <!-- Bootstrap 3.3.6 -->
+  <script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
+  <!-- iCheck -->
+  <script src="<?php echo base_url() ?>assets/plugins/iCheck/icheck.min.js"></script>
+  <!-- bootstrap datepicker -->
+  <script src="<?php echo base_url() ?>assets/plugins/datepicker/bootstrap-datepicker.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
 
 
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
+  <script>
+    $(function() {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+      });
     });
-  });
 
     //Date picker
     $('#datepicker').datepicker({
@@ -98,12 +102,12 @@
       viewMode: "years",
       minViewMode: "years"
     });
-    
+
     //Mask NIP
-    $(function () {
+    $(function() {
       $('#nip').mask('00000000 000000 0 000');
     });
-
-</script>
+  </script>
 </body>
+
 </html>
