699d0861244c227ada86e23f8f889e953a703dd1
anantakdewi <ananta.kusumadewi@gmail.com>
2020-05-30 11:55:56 +0800

    arsip fix

--- application/views/dashboard/petugas/arsip/arsip_surat.php
+++ application/views/dashboard/petugas/arsip/arsip_surat.php
@@ -1,30 +1,9 @@
 <!-- Main content petugas/arsip/arsip_surat.php VIEWS -->
 <section class="content">
     <div class="row">
-        <div class="col-md-3">
-            <div class="box box-solid">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Arsip</h3>
 
-                    <div class="box-tools">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="box-body no-padding">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li class=""><a href="<?php echo base_url('petugas/arsip') ?>"><i class="fa fa-inbox"></i>Ijazah dan Transkrip
-                                <span class="label label-primary pull-right"></span></a></li>
-                        <li><a href="<?php echo base_url('petugas/arsip/surat') ?>"><i class="fa fa-file-text-o"></i>Surat</a></li>
-                    </ul>
-                </div>
-                <!-- /.box-body -->
-            </div>
-
-            <!-- /.box -->
-        </div>
         <!-- /KONTEN -->
-        <div class="col-md-9">
+        <div class="col">
             <div class="box box-primary">
                 <div class="box-header with-border">
                     <div class="box-header with-border">
@@ -34,9 +13,9 @@
 
                     <div class="container">
                         <div class="row ">
-                            <div class="col-md-8">
+                            <div class="col-md-10">
                                 <form action="" method="POST">
-                                    <div class="input-group input-group-sm col-md-6">
+                                    <div class="input-group input-group-sm col-md-4">
                                         <input type="text" class="form-control" placeholder="ketik di sini.." name="cari_dok" autocomplete="off" autofocus>
                                         <span class="input-group-btn">
                                             <button type="button" class="btn btn-info btn-flat">Cari</button>
@@ -47,7 +26,7 @@
                                 <table class="table">
                                     <thead>
                                         <tr>
-                                            <th style="width: 15px">No</th>
+                                            <th style="width: 30px">No</th>
                                             <th scope="col">Jenis Surat</th>
                                             <!-- Nama surat yang di permohonan Lainnya , nama_permohonan di tabel permohonan -->
                                             <th scope="col">Nama Pemohon</th>
